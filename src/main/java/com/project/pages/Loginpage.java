a7974bf621be6d04ce6534dd66c154fb9788ce18
mkj <madhukiran918@gmail.com>
2022-12-30 02:22:49 +0530

    second

--- src/main/java/com/project/pages/Loginpage.java
+++ src/main/java/com/project/pages/Loginpage.java
@@ -5,12 +5,12 @@ import com.project.driver.DriverManager;
 import org.openqa.selenium.By;
 
 public class Loginpage {
-    private  final
+    private static final
     By txtboxUsername= By.xpath("//div/input[@name='username']");
-    private  final
+    private static final
     By txtboxPassword=By.xpath("//div/input[@name='password']");
-    private  final By btnSubmit=
-            By.xpath("//button[@type='submit']");
+    private static final
+    By btnSubmit= By.xpath("//button[@type='submit']");
 
     public  void setTxtboxUsername(String username){
         DriverManager.getDriver().findElement(txtboxUsername).sendKeys(username);
